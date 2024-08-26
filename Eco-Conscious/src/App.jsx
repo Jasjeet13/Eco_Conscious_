00d05b7d884f0e4b41752982613815ead6402730
Kashika2004s <kashikassdn28@gmail.com>
2024-08-26 19:37:10 +0530

    Kashika login2

--- Eco-Conscious/src/App.jsx
+++ Eco-Conscious/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Signup from "./signup";
+import Login from "./Login";
 
 function App() {
   return (
     <>
       <Signup></Signup>
+      <Login></Login>
+      
     </>
   );
 }
