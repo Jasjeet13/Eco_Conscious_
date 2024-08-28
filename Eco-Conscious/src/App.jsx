a425173409efc009a6c893d703c61b18df53e01e
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-08-28 14:12:30 +0530

    kanchan

--- Eco-Conscious/src/App.jsx
+++ Eco-Conscious/src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 // import SignUp_Login from "./Components/SignUp_Login";
 // import Login from "./Login";
 // import ProfileDetails from "./Components/ProfileDetails";
-import SignUp from "./Components/SignUp";
+// import SignUp from "./Components/SignUp";
+import NewArrivals from "./Components/navbar";
 function App() {
   return (
     <>
       {/* <Signup></Signup>
       <Login></Login> */}
       {/* <SignUp_Login></SignUp_Login> */}
-      <SignUp></SignUp>
+      {/* <SignUp></SignUp> */}
       {/* <ProfileDetails></ProfileDetails> */}
+      <NewArrivals></NewArrivals>
     </>
   );
 }
