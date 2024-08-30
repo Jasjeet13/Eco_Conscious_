ed76da1b03214898a89a37e6efc85d11054e0b74
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-08-30 20:45:32 +0530

    kanchan

--- Eco-Conscious/src/App.jsx
+++ Eco-Conscious/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import SignUp from "./Components/SignUp";
@@ -24,27 +23,6 @@ function App() {
     <div>
       <Edit />
     </div>
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignUp from './Components/SignUp';
-import Home from './components/Home';
-import SignUp_Login from './Components/SignUp_Login';
-import ProfileDetails from './Components/ProfileDetails';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUp_Login/>} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/home/:id" element={<Home />} /> 
-        <Route path="/profile/:id" element={<ProfileDetails />} />
-
-        {/* Add other routes here */}
-      </Routes>
-    </Router>
->>>>>>> 83f67317ec9ddb3a0bc09b2a137f93bfe1856fb7
   );
 }
 
