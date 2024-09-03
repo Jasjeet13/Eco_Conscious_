3d0a80003083be759adfbd6766cd9680191c25d7
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-09-03 08:03:50 +0530

    profiledetails

--- SignUp/routes/profile.js
+++ SignUp/routes/profile.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
 // Route to get the user profile by email
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
   console.log(`Received request for user profile with email: ${id}`);
 
   try {
-    // Find the user by email
+    // Find the user by id
     const user = await User.getUserProfileById(id);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found',id: user.id });
+      return res.status(404).json({ message: "User not found", id: user.id });
     }
 
     // Send the user data as JSON
@@ -24,8 +24,8 @@ router.get('/:id', async (req, res) => {
       mobileNumber: user.phoneNumber,
     });
   } catch (error) {
-    console.error('Error fetching user profile:', error);
-    res.status(500).json({ message: 'Error fetching profile details' });
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ message: "Error fetching profile details" });
   }
 });
 
