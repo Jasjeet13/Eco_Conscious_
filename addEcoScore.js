7a95ca5fc833089fe7220f2a03c2f39b0e89ca8d
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-11-28 08:21:52 +0530

    kanchan

--- addEcoScore.js
+++ addEcoScore.js
@@ -10,57 +10,41 @@ const calculateEcoScore = (product) => {
   const recyclability = product.recyclability || 0;
   const waterUsage = product.waterUsage
     ? product.waterUsage.toLowerCase()
-    : "moderate";
+    : "low";
   const energyEfficiency = product.energyEfficiency
     ? product.energyEfficiency.toLowerCase()
-    : "moderate";
+    : "low";
   const biodegradability = product.biodegradability || 0;
   const durability = product.durability || "0 months";
-
-  // Carbon footprint 0-100
-  score += carbonFootprint * 0.2; // 20% weightage in ecoscore
-
-  // Material sourcing good, better, best
+  score += carbonFootprint * 0.2;
   const materialSourcingScores = { good: 40, better: 70, best: 100 };
-  score += materialSourcingScores[materialSourcing] * 0.2; // 20% weightage in ecoscore
-
-  // Recyclability 0-100
-  score += recyclability * 0.2; // 20% weightage in ecoscore
-
-  // Water usage high, moderate, low
+  score += materialSourcingScores[materialSourcing] * 0.2;
+  score += recyclability * 0.2;
   const waterUsageScores = { high: 30, moderate: 60, low: 100 };
-  score += waterUsageScores[waterUsage] * 0.1; // 10% weightage in ecoscore
-
-  // Energy efficiency high, moderate, low
+  score += waterUsageScores[waterUsage] * 0.1;
   const energyEfficiencyScores = { high: 100, moderate: 70, low: 40 };
-  score += energyEfficiencyScores[energyEfficiency] * 0.1; // 10% weightage in ecoscore
-
-  // Biodegradability 0-100
-  score += biodegradability * 0.1; // 10% weightage in ecoscore
-
-  // Durability months or years, normalize to months
+  score += energyEfficiencyScores[energyEfficiency] * 0.1;
+  score += biodegradability * 0.1;
   const durabilityInMonths = durability.includes("month")
-    ? parseInt(durability) // months
-    : parseInt(durability) * 12; // years to months
-  const durabilityScore = Math.min(durabilityInMonths / 12, 1) * 10; // 10% weightage in ecoscore
+    ? parseInt(durability)
+    : parseInt(durability) * 12;
+  const durabilityScore = Math.min(durabilityInMonths / 12, 1) * 10;
   score += durabilityScore;
-
-  return parseFloat(Math.min(score, 100).toFixed(2)); //score does not exceed 100, rounded to 2 decimal places
+  return parseFloat(Math.min(score, 100).toFixed(2));
 };
-
 
 fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading the file:", err);
     return;
   }
-
   try {
     const jsonData = JSON.parse(data);
     const updatedData = jsonData.map((product) => {
       const ecoScore = calculateEcoScore(product);
       return { ...product, ecoScore };
     });
+
     fs.writeFile(filePath, JSON.stringify(updatedData, null, 2), (err) => {
       if (err) {
         console.error("Error writing to the file:", err);
