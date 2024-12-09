dcdb63e962de17539ee17810100c767aa88fb80b
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-12-09 15:28:57 +0530

    kanchan

--- SignUp/index.js
+++ SignUp/index.js
@@ -26,15 +26,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || process.env.PORT || 3000;
 
-
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecommerce", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB Connection Error:", error.message));
-
 
 app.use(
   cors({
@@ -47,7 +45,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 app.use("/api/profile", authenticateToken, profileRouter);
@@ -55,16 +52,15 @@ app.use("/api/products", authenticateToken, productsRouter);
 app.use("/api/edit", authenticateToken, editRouter);
 app.use("/api/delete", authenticateToken, deleteRouter);
 app.use("/api/wishlist", authenticateToken, wishlistRouter); // Wishlist route
-app.use('/api/cart', authenticateToken, cartRouter); // Cart route
-app.use("/api/order", authenticateToken, orderRoutes); 
-app.use("/api/search", searchRouter); 
-app.use("/api/alternatives", alternativeRouter); 
-app.use("/api/order-history",authenticateToken,orderhistoryRoutes);
-app.use("/api/bestproduct",authenticateToken,bestProductRouter);
-app.use('/verify', verifyRouter);
+app.use("/api/cart", authenticateToken, cartRouter); // Cart route
+app.use("/api/order", authenticateToken, orderRoutes);
+app.use("/api/search", searchRouter);
+app.use("/api/alternatives", alternativeRouter);
+app.use("/api/order-history", authenticateToken, orderhistoryRoutes);
+app.use("/api/bestproduct", authenticateToken, bestProductRouter);
+app.use("/verify", verifyRouter);
 
 app.use(errorHandler);
-
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
--- SignUp/models/wishlist.js
+++ SignUp/models/wishlist.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  
+
   name: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String },
   description: { type: String },
 });
 
-module.exports = mongoose.model('Wishlist', wishlistSchema);
-
-
-
+module.exports = mongoose.model("wishlist", wishlistSchema);
